24058c1fcd91343caba5512680a40ce14db3da77
kasimg <kasimgy@163.com>
2019-07-16 18:53:59 +0800

    upload

--- app/secure.py
+++ app/secure.py
@@ -3,10 +3,10 @@ Created by kasim on 2019/7/8 19:30
 存放机密信息
 git ignore掉
 """
-DEBUG = True
+DEBUG = False
 
 #  数据库连接信息机密信息，需要放在此处
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:kasim@localhost:3306/fish_book'
+SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:wjdh88166016@localhost:3306/fish_book'
 
 SECRET_KEY = 'ASDF93W49DSFYDV79498USDYF984W987SD8YFASHWER'
 
